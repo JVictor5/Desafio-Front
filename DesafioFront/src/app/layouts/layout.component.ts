db7ec1076efa5293138e4ccc666f9f7329dee5e2
Victor <josevictor_caravantealves10@hotmail.com>
2024-08-29 15:12:23 -0300

    feat: NavBar

--- DesafioFront/src/app/layouts/layout.component.ts
+++ DesafioFront/src/app/layouts/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   standalone: true,
   selector: 'app-layouts',
   templateUrl: './layout.component.html',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
 })
 export class LayoutComponent {}
